5eabf8c4aa3e0562f2bfc19782e38f190d513dc3
C0A23116 <c0a2311697@edu.teu.ac.jp>
2024-07-16 12:20:41 +0900

    チャージスキルの実装

--- HADOkokaton.py
+++ HADOkokaton.py
@@ -275,6 +275,167 @@ class Explosion(pg.sprite.Sprite):
         if self.life < 0:
             self.kill()
         
+class Skill_cut_1(pg.sprite.Sprite):
+    """
+    キャラに対応したスキルを発動
+    """
+    def __init__(self, life, num, screen: pg.Surface):
+        super().__init__()
+        self.life = life
+        self.image = pg.Surface((WIDTH, HEIGHT))
+        self.rect = self.image.get_rect()
+        pg.draw.rect(self.image, (0, 0, 0), (WIDTH/2, 0, 290, 190))
+        pg.draw.polygon(self.image, (255,255,255), [[630, 220], [865, 160], [900, 165], [935, 145], [WIDTH, 110],[WIDTH,260], [1050, 250], [970, 280], [630, 295], [500,280]]) 
+        pg.draw.polygon(self.image, (0,0,255), [[630, 230], [865, 170], [900, 175], [935, 155], [WIDTH, 120],[WIDTH,240], [1050, 240], [970, 270], [630, 285], [530,275]])       
+        #pg.draw.rect(self.image, (0, 0, 0), (WIDTH/2, 0, 290, 190))
+        #pg.draw.rect(self.image, (0, 0, 255), (WIDTH/2, 0, 270, 170))
+        self.image.set_alpha(200)
+        self.image_cut = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 5)
+        self.rect_cut = self.image_cut.get_rect()
+
+    def update(self, screen):
+        self.life -= 1
+        screen.blit(self.image_cut, (WIDTH*5/7, HEIGHT*2/9))
+        if self.life < 0:
+            self.kill()
+        if check_bound(self.rect) != (True, True):
+            self.kill()
+
+class Skill_1(pg.sprite.Sprite):
+    """
+    キャラクター１のスキルに関するクラス
+    """
+    def __init__(self, chara: Chara_1):
+        """
+        スキル画像Surfaceを生成する
+        引数 chara_1：スキルを放つキャラクター
+        """
+        super().__init__()
+        self.vx, self.vy = chara.dire
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.image = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), angle, 1.5)
+        self.vx = math.cos(math.radians(angle))
+        self.vy = -math.sin(math.radians(angle))
+        self.rect = self.image.get_rect()
+        self.rect.centery = chara.rect.centery+chara.rect.height*self.vy
+        self.rect.centerx = chara.rect.centerx+chara.rect.width*self.vx
+        self.speed = 100
+
+    def update(self):
+        """
+        スキルを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        self.rect.move_ip(self.speed*self.vx, self.speed*self.vy)
+        if check_bound(self.rect) != (True, True):
+            self.kill()
+
+class Skillpoint_1:
+    """
+    時間経過と共に上昇するスキルポイント
+    """
+    def __init__(self):
+        self.value = 0
+        self.gauge_img0_1 = pg.transform.rotozoom(pg.image.load(f"fig/{3}.png"), 0, 1)
+
+    def update(self):
+        if (self.value < 100):
+            self.value += 0.25
+
+    def draw_gauge(self, surface, x, y, radius_1, current_1, maximum_1):
+
+        # ゲージの背景（白で円を描画）
+        pg.draw.circle(surface, (255, 255, 255), (x, y), radius_1)
+
+        # 充填部分（青で扇形を描画）
+        angle_1 = (current_1 / maximum_1) * 360  # 充填率を角度に変換
+        start_angle_1 = 90  #90度から描画する
+        end_angle_1 = start_angle_1 + 270/360 * angle_1
+        pg.draw.arc(surface, (0, 0, 255), (x - radius_1, y - radius_1, 2 * radius_1, 2 * radius_1), math.radians(start_angle_1), math.radians(end_angle_1), width=radius_1)
+        pg.draw.circle(surface, (0, 0, 0), (x, y), radius_1/2)
+        pg.draw.rect(surface, (0, 0, 0), (x, y - radius_1, radius_1, radius_1))
+        surface.blit(self.gauge_img0_1, (x, y - radius_1))
+
+class Skill_cut_2(pg.sprite.Sprite):
+    """
+    キャラに対応したスキルを発動
+    """
+    def __init__(self, life, num, screen: pg.Surface):
+        super().__init__()
+        self.life = life
+        self.image = pg.Surface((WIDTH, HEIGHT))
+        self.rect = self.image.get_rect()
+        pg.draw.rect(self.image, (0, 0, 0), (WIDTH/2, 0, 290, 190))
+        pg.draw.polygon(self.image, (255,255,255), [[630, 220], [865, 160], [900, 165], [935, 145], [WIDTH, 110],[WIDTH,260], [1050, 250], [970, 280], [630, 295], [500,280]]) 
+        pg.draw.polygon(self.image, (0,0,255), [[630, 230], [865, 170], [900, 175], [935, 155], [WIDTH, 120],[WIDTH,240], [1050, 240], [970, 270], [630, 285], [530,275]])       
+        #pg.draw.rect(self.image, (0, 0, 0), (WIDTH/2, 0, 290, 190))
+        #pg.draw.rect(self.image, (0, 0, 255), (WIDTH/2, 0, 270, 170))
+        self.image.set_alpha(200)
+        self.image_cut = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 5)
+        self.rect_cut = self.image_cut.get_rect()
+
+    def update(self, screen):
+        self.life -= 1
+        screen.blit(self.image_cut, (WIDTH*5/7, HEIGHT*2/9))
+        if self.life < 0:
+            self.kill()
+        if check_bound(self.rect) != (True, True):
+            self.kill()
+
+class Skill_2(pg.sprite.Sprite):
+    """
+    キャラクター１のスキルに関するクラス
+    """
+    def __init__(self, chara: Chara_2):
+        """
+        スキル画像Surfaceを生成する
+        引数 chara_2：スキルを放つキャラクター
+        """
+        super().__init__()
+        self.vx, self.vy = chara.dire
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.image = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), angle, 1.5)
+        self.vx = math.cos(math.radians(angle))
+        self.vy = -math.sin(math.radians(angle))
+        self.rect = self.image.get_rect()
+        self.rect.centery = chara.rect.centery+chara.rect.height*self.vy
+        self.rect.centerx = chara.rect.centerx+chara.rect.width*self.vx
+        self.speed = 100
+
+    def update(self):
+        """
+        スキルを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        self.rect.move_ip(self.speed*self.vx, self.speed*self.vy)
+        if check_bound(self.rect) != (True, True):
+            self.kill()
+
+class Skillpoint_2:
+    """
+    時間経過と共に上昇するスキルポイント
+    """
+    def __init__(self):
+        self.value = 0
+        self.gauge_img0_2 = pg.transform.rotozoom(pg.image.load(f"fig/{32}.png"), 0, 1)
+
+    def update(self):
+        if (self.value < 100):
+            self.value += 0.25
+
+    def draw_gauge(self, surface, x, y, radius_2, current_2, maximum_2):
+
+        # ゲージの背景（白で円を描画）
+        pg.draw.circle(surface, (255, 255, 255), (x, y), radius_2)
+
+        # 充填部分（青で扇形を描画）
+        angle_2 = (current_2 / maximum_2) * 360  # 充填率を角度に変換
+        start_angle_2 = 90  #90度から描画する
+        end_angle_2 = start_angle_2 + 270/360 * angle_2
+        pg.draw.arc(surface, (0, 0, 255), (x - radius_2, y - radius_2, 2 * radius_2, 2 * radius_2), math.radians(start_angle_2), math.radians(end_angle_2), width=radius_2)
+        pg.draw.circle(surface, (0, 0, 0), (x, y), radius_2/2)
+        pg.draw.rect(surface, (0, 0, 0), (x, y - radius_2, radius_2, radius_2))
+        surface.blit(self.gauge_img0_2, (x, y - radius_2))
 
 def main():
     pg.display.set_caption("HADOU!!こうかとん!!")
@@ -286,6 +447,16 @@ def main():
     beams1 = pg.sprite.Group()
     beams2 = pg.sprite.Group()
     exps = pg.sprite.Group()
+    skill1 = pg.sprite.Group()
+    skill2 = pg.sprite.Group()
+
+    skillpoint1 = Skillpoint_1()
+    skill_gauge_value_1 = 0
+    max_value_1 = 100
+    skillpoint2 = Skillpoint_2()
+    skill_gauge_value_2 = 0
+    max_value_2 = 100
+
 
     tmr = 0
     clock = pg.time.Clock()
@@ -319,6 +490,45 @@ def main():
             time.sleep(2)
             return
 
+        #chara1の必殺技
+        if skill_gauge_value_1 == 100 and event.type == pg.KEYDOWN and event.key == pg.K_RCTRL:
+            #skill.add(Skill_1(35, 300, screen))
+            skill1.add(Skill_1(charas1))# スキル発射！
+            skill_gauge_value_1 = 0
+        
+        #ゲージの変化
+        if skill_gauge_value_1 != max_value_1: # ゲージを増やす
+            skill_gauge_value_1 += 0.25
+
+        # chara2とchara1スキルの当たり判定
+        if len(pg.sprite.spritecollide(charas2, skill1, True)) != 0:
+            exps.add(Explosion(charas2, 100))  # 爆発エフェクト
+            charas1.change_img(6, screen)  # こうかとん喜びエフェクト
+            charas2.change_img(82, screen) # こうかとん悲しみエフェクト
+            pg.display.update()
+            time.sleep(2)
+            return
+        
+        #chara2の必殺技
+        if skill_gauge_value_2 == 100 and event.type == pg.KEYDOWN and event.key == pg.K_LCTRL:
+            #skill.add(Skill_2(35, 300, screen))
+            skill2.add(Skill_2(charas2))# スキル発射！
+            skill_gauge_value_2 = 0
+        
+        #ゲージの変化
+        if skill_gauge_value_2 != max_value_2: # ゲージを増やす
+            skill_gauge_value_2 += 0.25
+
+        # chara1とchara2スキルの当たり判定
+        if len(pg.sprite.spritecollide(charas1, skill2, True)) != 0:
+            exps.add(Explosion(charas1, 100))  # 爆発エフェクト
+            charas2.change_img(6, screen)  # こうかとん喜びエフェクト
+            charas1.change_img(82, screen) # こうかとん悲しみエフェクト
+            pg.display.update()
+            time.sleep(2)
+            return
+
+
         # 座標確認用(座標を確認したいときに使ってね！)
         #line = pg.Surface((WIDTH, HEIGHT))
         #pg.draw.line(screen, (255, 0, 0), (0, 680), (WIDTH, 680), 2)
@@ -331,6 +541,12 @@ def main():
         beams2.draw(screen)
         exps.update()
         exps.draw(screen)
+        skillpoint1.draw_gauge(screen, WIDTH -50, 70, 50, skill_gauge_value_1, max_value_1)
+        skill1.update()
+        skill1.draw(screen)
+        skillpoint2.draw_gauge(screen, 50, 70, 50, skill_gauge_value_2, max_value_2)
+        skill2.update()
+        skill2.draw(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
